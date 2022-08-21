3f421ed0310b79c88bf5f4ae0c117c86845bc10a
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-21 16:59:12 +0200

    Get files from args

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,25 @@ mod json;
 mod utils;
 
 use std::io::Error;
-
+use std::path::Path;
+use std::{env, process};
 use json::Json;
+use std::thread;
 
 fn main() -> Result<(), Error> {
-    let json = Json::from_file("example.toml")?;
-    let result = json.save()?;
+    let files = env::args().skip(1).collect::<Vec<String>>();
 
-    println!("{:#?}", result);
+    if files.len() == 0 {
+        eprintln!("No files provided");
+        process::exit(0);
+    }
+
+    files.into_iter().map(|f| {
+        thread::spawn(|| {
+            let json = Json::from_file(f).unwrap();
+            json.save().expect("Unable to save json file");
+        })
+    }).for_each(|h| h.join().unwrap());
+
     Ok(())
 }
